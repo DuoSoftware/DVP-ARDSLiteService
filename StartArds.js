ab55bc46283603d7d680ac759bf1e28a553ce9b7
heshanindika <k.heshan.indika@gmail.com>
2015-08-12 11:48:58 +0530

    add JS files

---
+++ StartArds.js
@@ -0,0 +1,75 @@
+﻿var util = require('util');
+var redisHandler = require('ArdsCommon/RedisHandler.js')
+var sortArray = require('ArdsCommon/SortArray.js');
+var reqQueueHandler = require('ArdsCommon/ReqQueueHandler.js');
+var resourceHandler = require('ArdsCommon/ResourceHandler.js');
+var preProcessHandler = require('ArdsCommon/PreProcessor.js');
+var contArdsHandler = require('./ContinueArdsProcess.js');
+var infoLogger = require('ArdsCommon/InformationLogger.js');
+var requestHandler = require('ArdsCommon/RequestHandler.js');
+
+var AddRequest = function (logKey, reqPreObj, callback) {
+    preProcessHandler.execute(logKey, reqPreObj, function (err, requestObj) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            infoLogger.DetailLogger.log('info', '%s ************************* Start AddRequest *************************', logKey);
+            
+            var key = util.format('Request:%d:%d:%s', requestObj.Company, requestObj.Tenant, requestObj.SessionId);
+            var tag = ["company_" + requestObj.Company, "tenant_" + requestObj.Tenant, "class_" + requestObj.Class, "type_" + requestObj.Type, "category_" + requestObj.Category, "sessionid_" + requestObj.SessionId, "reqserverid_" + requestObj.RequestServerId, "priority_" + requestObj.Priority, "servingalgo_" + requestObj.ServingAlgo, "handlingalgo_" + requestObj.HandlingAlgo, "selectionalgo_" + requestObj.SelectionAlgo, "objtype_Request"];
+            
+            var tempAttributeList = [];
+            for (var i in requestObj.AttributeInfo) {
+                var atts = requestObj.AttributeInfo[i].AttributeCode;
+                for (var j in atts) {
+                    tempAttributeList.push(atts[j]);
+                }
+            }
+            var sortedAttributes = sortArray.sortData(tempAttributeList);
+            for (var k in sortedAttributes) {
+                tag.push("attribute_" + sortedAttributes[k]);
+            }
+            
+            var jsonObj = JSON.stringify(requestObj);
+            redisHandler.AddObj_V_T(logKey, key, jsonObj, tag, function (err, reply, vid) {
+                if (err) {
+                    console.log(err);
+                    callback(err, null, 0);
+                }
+                else {
+                    requestHandler.SetRequestState(logKey, requestObj.Company, requestObj.Tenant, requestObj.SessionId, "N/A", function (err, result) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    });
+                    switch (requestObj.ReqHandlingAlgo) {
+                        case "QUEUE":
+                            reqQueueHandler.AddRequestToQueue(logKey, requestObj, function (err, result) {
+                                if (err) {
+                                    console.log(err);
+                                    callback(err, "Add Request to Queue Failed. sessionId :: " + requestObj.SessionId, vid);
+                                }
+                                else {
+                                    console.log("Request added to queue. sessionId :: " + requestObj.SessionId);
+                                    callback(err, "Request added to queue. sessionId :: " + requestObj.SessionId, vid);
+                                }
+                            });
+                            break;
+                        case "DIRECT":
+                            contArdsHandler.ContinueArds(requestObj, function (handlingResource) {
+                                callback(err, handlingResource, vid);
+                            });
+                            break;
+                        default:
+                            callback(err, "No ReqHandlingAlgo Found.", vid);
+                            break;
+                    }
+                }
+            });
+        }
+    });
+};
+
+
+module.exports.AddRequest = AddRequest;
---
+++ Test.js
@@ -0,0 +1,145 @@
+﻿//var restClientHandler = require('./RestClient.js');
+
+//restClientHandler.DoGet("http://localhost:2225", "/requestserver/get/1/3/1", function () {
+//});
+
+//var pd = "{\"Company\": 1,\"Tenant\": 3,\"Class\": \"TESTERVER\",\"Type\": \"ARDS\",\"Category\": \"CALL\",\"CallbackUrl\": \"http://localhost:5426/Callback\",\"ServerID\": 2}";
+//restClientHandler.DoPost("http://localhost:2225/requestserver/add", JSON.parse(pd), function () {
+//});
+
+//var configHandler = require('./Config.json');
+//console.log(configHandler.basicSelectionUrl);
+//console.log("");
+//var util = require('util');
+//var key = "Resource:1:3:555555555";
+//var lockKey = util.format('%s', key.split(":").join(""));
+//console.log("key:: " + key);
+//console.log("lockKey:: " + lockKey);
+
+//var restify = require('restify');
+
+//var server = restify.createServer({
+//    name: 'Test2',
+//    version: '1.0.0'
+//});
+//var internalserver = restify.createServer({
+//    name: 'internalserverArdsServer',
+//    version: '1.0.0'
+//});
+//server.use(restify.acceptParser(server.acceptable));
+//server.use(restify.queryParser());
+//server.use(restify.bodyParser());
+
+//server.post('/callback/print', function (req, res, next) {
+//    var resString = JSON.stringify(req.body);
+//    console.log(resString);
+//    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+//    res.end("true");
+//    return next();
+//});
+
+//server.listen(2228, function () {
+//    console.log('%s listening at %s', server.name, server.url);
+//});
+
+
+///Winston
+
+
+//var winston = require('winston');
+//var config = require('./Config.json');
+//var fs = require('fs');
+
+//var logger = module.exports = new (winston.Logger)({
+//    transports: [
+//        new (winston.transports.Console)({
+//            colorize: 'all',
+//            level:'debug'
+//        })
+//    ]
+//});
+
+//var logger = new (winston.Logger)({
+//    transports: [
+//        new (winston.transports.File)({
+//            filename: config.winston.filename,
+//            level: config.winston.level,
+//            json: config.winston.json,
+//            maxsize: config.winston.maxsize,
+//            maxFiles: config.winston.maxFiles
+//        })
+//    ]
+//});
+
+//logger.log('debug', 'This is an information message.');
+//logger.log('info', 'This is an information message.');
+//logger.log('error', 'This is an information message.');
+//logger.debug('HelloWorld');
+
+//var logger = new (winston.Logger)({
+//    transports: [
+//        new (winston.transports.Console)({ raw: true }),
+//    ]
+//});
+
+//logger.log('info', 'Hello, this is a raw logging event', { 'foo': 'bar' });
+////logger.log('info', 'Hello, this is a raw logging event 2', { 'foo': 'bar' });
+
+//var path = require('path');
+
+//var filename1 = path.join('C:/IISLogs/Ards_v6/NodeJs/Init', 'log.duo');
+//var filename2 = path.join('C:/IISLogs/Ards_v6/NodeJs/Cont', 'log.duo');
+
+//
+// Remove the file, ignoring any errors
+//
+
+//var result1 = fs.existsSync('C:/IISLogs/Ards_v6/NodeJs/Init');
+//if (result1 == false) {
+//    try {
+//        var rrr = fs.mkdirSync('C:/IISLogs/Ards_v6/NodeJs/Init');
+//    }
+//    catch (ex) { 
+//        console.log(ex);
+//    }
+//}
+
+//var result2 = fs.existsSync('C:/IISLogs/Ards_v6/NodeJs/Cont');
+//if (result2 == false) {
+//    try { fs.mkdirSync('C:/IISLogs/Ards_v6/NodeJs/Cont'); }
+//    catch (ex) { }
+//}
+
+//
+// Create a new winston logger instance with two tranports: Console, and File
+//
+//
+//var logger = new (winston.Logger)({
+//    transports: [
+//        new (winston.transports.Console)(),
+//        new (winston.transports.File)({ name: 'Init-file', filename: filename1 }),
+//        new (winston.transports.File)({ name: 'Cont-file', filename: filename2 })
+//    ]
+//});
+
+//logger.
+
+//logger.log('info', 'Hello created log files!', { 'foo': 'bar' });
+
+
+
+
+//var infoLogger = require('./InformationLogger.js');
+
+//infoLogger.log('debug', 'This is an information message.');
+//infoLogger.log('info', 'This is an information message.');
+//infoLogger.log('error', 'This is an information message.');
+//infoLogger.debug('HelloWorld');
+
+
+var uuid = require('node-uuid');
+var util = require('util');
+
+var key = util.format('[%s]::[%s]', uuid.v1(), uuid.v1());
+            
+console.log(key);
