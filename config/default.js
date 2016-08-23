fda72595b59fa0eb1a16bfbd27c218c1c27e5fa4
heshanindika <k.heshan.indika@gmail.com>
2016-08-23 12:07:36 +0530

    update ardsCommon 1.1.38

--- config/default.js
+++ config/default.js
@@ -6,11 +6,11 @@
 		"password":"DuoS123"
 	},
 	"Services" : {
-		"accessToken":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiMTdmZTE4M2QtM2QyNC00NjQwLTg1NTgtNWFkNGQ5YzVlMzE1Iiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE4OTMzMDI3NTMsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NjEyOTkxNTN9.YiocvxO_cVDzH5r67-ulcDdBkjjJJDir2AeSe3jGYeA",
-		"routingServiceHost": "ardsliteroutingengine.104.131.67.21.xip.io",
+		"accessToken":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo",
+		"routingServiceHost": "ardsliteroutingengine.app.veery.cloud",
 		"routingServicePort": "2223",
 		"routingServiceVersion": "1.0.0.0",
-		"resourceServiceHost": "resourceservice.104.131.67.21.xip.io",
+		"resourceServiceHost": "resourceservice.app.veery.cloud",
 		"resourceServicePort": "8831",
 		"resourceServiceVersion": "1.0.0.0"
 	},
