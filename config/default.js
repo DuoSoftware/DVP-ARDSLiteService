e48fd9844c2a7586c77698c19f95276dcfa6db35
sharonsahadevan <sharonsujee36@gmail.com>
2020-06-04 17:08:02 +0530

    redis conf updated

--- config/default.js
+++ config/default.js
@@ -2,14 +2,14 @@
     "Redis":
         {
             "mode":"instance",//instance, cluster, sentinel
-            "ip": "138.197.90.92",
-            "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
+            "ip": "",
+            "port": 6379,
+            "user": "",
+            "password": "",
             "db":6,
             "sentinels":{
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port":16389,
+                "hosts": "",
+                "port":6379,
                 "name":"redis-cluster"
             }
 
@@ -55,14 +55,14 @@
     "Security":
         {
 
-            "ip" : "138.197.90.92",
-            "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
+            "ip" : "",
+            "port": 6379 ,
+            "user": "",
+            "password": "",
             "mode":"instance",//instance, cluster, sentinel
             "sentinels":{
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port":16389,
+                "hosts": "",
+                "port":6379,
                 "name":"redis-cluster"
             }
         },
